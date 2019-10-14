4e60fecbc29c57f494e845c69d3c12c64b1667d3
tyln <taylan.sbrcn@gmail.com>
2019-10-14 20:42:16 +0300

    api key moved to request interceptor

--- base/core_data/src/main/java/com/raqun/movies/core/data/api/DefaultRequestInterceptor.kt
+++ base/core_data/src/main/java/com/raqun/movies/core/data/api/DefaultRequestInterceptor.kt
@@ -6,11 +6,18 @@ import javax.inject.Inject
 
 private const val CONTENT_TYPE = "Content-Type"
 private const val JSON = "application/json"
+private const val KEY_API = "api_key"
 
 class DefaultRequestInterceptor @Inject constructor() : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request()
+            .url()
+            .newBuilder().addQueryParameter(KEY_API, ApiConstants.API_KEY)
+            .build()
+
         return chain.proceed(with(chain.request().newBuilder()) {
+            url(url)
             addHeader(CONTENT_TYPE, JSON)
             build()
         })
--- shows/shows_data/src/main/java/com/raqun/movies/shows/data/TvShowsServices.kt
+++ shows/shows_data/src/main/java/com/raqun/movies/shows/data/TvShowsServices.kt
@@ -1,6 +1,5 @@
 package com.raqun.movies.shows.data
 
-import com.raqun.movies.core.data.api.ApiConstants
 import com.raqun.movies.core.data.api.PagedApiResponse
 import com.raqun.movies.shows.domain.TvShow
 import com.raqun.movies.shows.domain.TvShowDetail
@@ -10,9 +9,9 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TvShowsServices {
-    @GET("/3/tv/popular?api_key=${ApiConstants.API_KEY}")
+    @GET("/3/tv/popular")
     fun getPopularTvShows(@Query("page") page: Int = 1): Single<PagedApiResponse<List<TvShow>>>
 
-    @GET("/3/tv/{tv_id}?api_key=${ApiConstants.API_KEY}")
+    @GET("/3/tv/{tv_id}")
     fun getTvShowDetail(@Path("tv_id") id: Int): Single<TvShowDetail>
 }
